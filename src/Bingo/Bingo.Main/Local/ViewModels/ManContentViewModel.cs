0d69c3fce1d6d08b25faac228987de2b58a87437
이광석 <kevin.lgs@aceenter.com>
2023-04-13 17:42:56 +0900

    디펜더시 'Columns' 제거

--- src/Bingo/Bingo.Main/Local/ViewModels/ManContentViewModel.cs
+++ src/Bingo/Bingo.Main/Local/ViewModels/ManContentViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using Bingo.Core.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Jamesnet.Wpf.Controls;
 using Jamesnet.Wpf.Mvvm;
+using System;
 
 namespace Bingo.Main.Local.ViewModels
 {
@@ -11,40 +13,49 @@ namespace Bingo.Main.Local.ViewModels
 				private BingoItemCollection bingoItems;
 				public ManContentViewModel()
 				{
-						bingoItems = new ()
+						this.BingoItems = new ()
 						{
-								new BingoItem("하이하이"),
-								new BingoItem("하이하이"),
-								new BingoItem("하이하이"),
-								new BingoItem("하이하이"),
-								new BingoItem("하이하이"),
-								new BingoItem("하이하이"),
-								new BingoItem("하이하이"),
-								new BingoItem("하이하이"),
-								new BingoItem("하이하이"),
-								new BingoItem("하이하이"),
-								new BingoItem("하이하이"),
-								new BingoItem("하이하이"),
-								new BingoItem("하이하이"),
-								new BingoItem("하이하이"),
-								new BingoItem("하이하이"),
-								new BingoItem("하이하이"),
-								new BingoItem("하이하이"),
-								new BingoItem("하이하이"),
-								new BingoItem("하이하이"),
-								new BingoItem("하이하이"),
-								new BingoItem("하이하이"),
-								new BingoItem("하이하이"),
-								new BingoItem("하이하이"),
-								new BingoItem("하이하이"),
-								new BingoItem("하이하이"),
+								new BingoItem("1"),
+								new BingoItem("2"),
+								new BingoItem("3"),
+								new BingoItem("4"),
+								new BingoItem("5"),
+								new BingoItem("6"),
+								new BingoItem("7"),
+								new BingoItem("8"),
+								new BingoItem("9"),
+								new BingoItem("10"),
+								new BingoItem("11"),
+								new BingoItem("12"),
+								new BingoItem("13"),
+								new BingoItem("14"),
+								new BingoItem("15"),
+								new BingoItem("16"),
+								new BingoItem("17"),
+								new BingoItem("18"),
+								new BingoItem("19"),
+								new BingoItem("20"),
+								new BingoItem("21"),
+								new BingoItem("22"),
+								new BingoItem("23"),
+								new BingoItem("24"),
+								new BingoItem("25"),
 						};
-						bingoItems.NumOfLine = 5;
+						this.BingoItems.NumOfLine = 5;
 				}
 
 				public void OnLoaded(IViewable view)
 				{
 
+				}
+
+				[RelayCommand]
+				private void Goal(string goalCount)
+				{
+						if (String.IsNullOrWhiteSpace (goalCount))
+								return;
+
+						this.BingoItems.Goal = Convert.ToInt32 (goalCount);
 				}
 		}
 }
