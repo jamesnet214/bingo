94e1b58e98985ec15696ccd454b011fb90b471d8
DH Shin <0041lntc@gmail.com>
2023-04-13 18:09:35 +0900

    드래그드랍을 이용한 아이템 순서 변경 추가

---
+++ src/Bingo/Bingo.LayoutSupport/Behaviors/DragDropAdorner.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows;
+
+namespace Bingo.LayoutSupport.Behaviors;
+public class DragDropAdorner : Adorner
+{
+	private Brush _visualBrush;
+
+	private Point _currentLocation;
+
+	private Point _startLocation;
+
+	public DragDropAdorner(UIElement adornedElement, Point startLocation) : base(adornedElement)
+	{
+		_startLocation = startLocation;
+		_visualBrush = new VisualBrush(AdornedElement)
+		{
+			Opacity = .7
+		};
+		IsHitTestVisible = false;
+	}
+
+	public void UpdatePosition(Point location)
+	{
+		_currentLocation = location;
+
+		InvalidateVisual();
+	}
+
+	protected override void OnRender(DrawingContext dc)
+	{
+		var p = _currentLocation;
+		p.Offset(-_startLocation.X, -_startLocation.Y);
+
+		dc.DrawRectangle(_visualBrush, null, new Rect(p, RenderSize));
+	}
+}
