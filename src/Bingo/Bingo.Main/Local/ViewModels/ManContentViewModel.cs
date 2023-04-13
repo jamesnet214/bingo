c994d6546e6360d5657f7c5bf75698d5500c093b
이광석 <kevin.lgs@aceenter.com>
2023-04-13 17:08:47 +0900

    컬럼 정렬값 추가

--- src/Bingo/Bingo.Main/Local/ViewModels/ManContentViewModel.cs
+++ src/Bingo/Bingo.Main/Local/ViewModels/ManContentViewModel.cs
@@ -8,9 +8,14 @@ namespace Bingo.Main.Local.ViewModels
 		public partial class ManContentViewModel : ObservableBase, IViewLoadable
 		{
 				[ObservableProperty]
+				private int columns = 0;
+				[ObservableProperty]
 				private BingoItemCollection bingoItems;
 				public ManContentViewModel()
 				{
+						#region TestCode
+						this.Columns = 5;
+						#endregion
 						bingoItems = new ()
 						{
 								new BingoItem("하이하이"),
--- src/Bingo/Bingo.Main/UI/Units/BingoListBox.cs
+++ src/Bingo/Bingo.Main/UI/Units/BingoListBox.cs
@@ -5,6 +5,19 @@ namespace Bingo.Main.UI.Units
 {
 		public class BingoListBox : ListBox
 		{
+
+
+				public int Columns
+				{
+						get { return (int)GetValue (ColumnsProperty); }
+						set { SetValue (ColumnsProperty, value); }
+				}
+
+				// Using a DependencyProperty as the backing store for Columns.  This enables animation, styling, binding, etc...
+				public static readonly DependencyProperty ColumnsProperty =
+					DependencyProperty.Register ("Columns", typeof (int), typeof (BingoListBox), new PropertyMetadata (0));
+
+
 				static BingoListBox()
 				{
 						DefaultStyleKeyProperty.OverrideMetadata (typeof (BingoListBox), new FrameworkPropertyMetadata (typeof (BingoListBox)));
